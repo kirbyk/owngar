a8efd734d2c519d21a471667201bd61aad7e820f
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2015-09-05 22:17:54 -0400

    Such react-canvasify

--- src/components/Virus.js
+++ src/components/Virus.js
@@ -1,11 +1,14 @@
-import Constants from '../constants';
 import Circle from './Circle';
+import Constants from '../constants';
+import { randBetween } from '../util';
 
 
 export default class Virus extends Circle {
 
-  constructor(context, props) {
-    super(context, props);
+  constructor(props) {
+    super(props);
+    this._xPos = randBetween(window.innerWidth);
+    this._yPos = randBetween(window.innerHeight);
     this._radius = Constants.virusRadius;
     this._color = Constants.virusColor;
   }
